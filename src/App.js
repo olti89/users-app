68005d8fd58b19715bcaf693de26e2a2c8b84b30
olti89 <149578000+olti89@users.noreply.github.com>
2025-10-03 03:11:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserList from "./components/UserList";
+import UserDetails from "./components/UserDetails";
 
 function App() {
   return (
