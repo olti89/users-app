74df9438bc636c24043c676f957a928f4f052032
olti89 <149578000+olti89@users.noreply.github.com>
2025-10-03 13:10:30 +0200

    Update UserDetails.js

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -11,3 +11,21 @@ function UserDetails() {
       .then((res) => res.json())
       .then((data) => setUser(data));
   }, [id]);
+
+  if (!user) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
+      <p>
+        Address: {user.address?.street}, {user.address?.city}
+      </p>
+      <button onClick={() => navigate("/")}>Kthehu</button>
+    </div>
+  );
+}
+
+export default UserDetails;
