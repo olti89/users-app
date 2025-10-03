c14ce92fe55097ccba7db5562becb6b77842a2a0
olti89 <149578000+olti89@users.noreply.github.com>
2025-10-03 13:09:33 +0200

    UserDetails.js

---
+++ src/components/UserDetails.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+function UserDetails() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, [id]);
